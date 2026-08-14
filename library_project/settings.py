bfc307ca533a3d1995d120fc0e3a2fe57325c2bf
mugasin01 <mugasin.m@edgezenlabs.com>
2026-08-14 09:46:44 +0530

    Update Django project settings

--- library_project/settings.py
+++ library_project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'library_db',
         'USER': 'postgres',
-        'PASSWORD': 'postgresql',  # your PostgreSQL password
+        'PASSWORD': 'postgresql',  # password
         'HOST': 'localhost',
         'PORT': '5432',
     }
